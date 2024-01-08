4ee220cec6a5948d73a3b6ced204c202e8c446a0
crongd <leejaeho1500@gmail.com>
2024-01-08 10:59:46 +0900

    complete

--- src/test/java/com/batchwritercsv/BatchWriterCsvApplicationTests.java
+++ src/test/java/com/batchwritercsv/BatchWriterCsvApplicationTests.java
@@ -1,13 +1,18 @@
 package com.batchwritercsv;
 
+import com.batchwritercsv.service.BatchService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BatchWriterCsvApplicationTests {
+    @Autowired
+    private BatchService batchService;
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        batchService.csv_writer();
     }
 
 }
